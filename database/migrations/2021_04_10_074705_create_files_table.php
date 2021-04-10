c1a5d3ac1ac8abfab4935d61271468109311d943
alpharameeztech <rameezisrarulhaq@gmail.com>
2021-04-10 13:04:25 +0500

    Add migrations

---
+++ database/migrations/2021_04_10_074705_create_files_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('storage_folder_id');
+            $table->uuid('name');
+            $table->string('convert_status')->default('in_progress');
+            $table->timestamps();
+
+            $table->foreign('storage_folder_id')
+                ->references('id')
+                ->on('storage_folders')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
